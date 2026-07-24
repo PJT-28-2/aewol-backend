956be13e94b11cc8e95069f376c75a145c2f9509
gnvvoo <t412@naver.com>
2026-07-24 10:02:57 +0900

    fix: AewolApplicationTests 컨텍스트 로딩 실패 수정

--- src/test/java/com/aewol/AewolApplicationTests.java
+++ src/test/java/com/aewol/AewolApplicationTests.java
@@ -2,11 +2,9 @@ package com.aewol;
 
 import com.aewol.config.AppConfig;
 import org.junit.jupiter.api.Test;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
 
-@SpringJUnitConfig(classes = AppConfig.class)
-@ContextConfiguration
+@SpringJUnitWebConfig(classes = AppConfig.class)
 class AewolApplicationTests {
 
     @Test
